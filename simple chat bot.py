6233539b97aa857c305852c6439d1eb83afaf595
soukarsha122 <78530146+soukarsha122@users.noreply.github.com>
2023-07-24 13:33:12 +0530

    Update simple chat bot.py

--- simple chat bot.py
+++ simple chat bot.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = "#####"
 
 completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[
                                           {"role": "user", "content": "Give me 3 ideas for apps I could build with openai apis "}])
